690fca41515714b2f8972369c4c3ca886e7c43b8
robert <voicu.eduardrobert@gmail.com>
2022-04-07 13:53:45 +0300

    execute sync concurrent

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -6,13 +6,13 @@ import (
 	"github.com/bigkevmcd/go-configparser"
 	"github.com/fatih/color"
 	"io"
-	"io/fs"
 	"log"
 	"nicm_client/app/consts"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -51,6 +51,8 @@ func UpdateVersion(newVersion string) {
 	}
 }
 
+var wg sync.WaitGroup
+
 func SyncArchives(config ConfigMap) {
 	archives, exists := config["ARCHIVES"]
 	if exists == false {
@@ -60,39 +62,57 @@ func SyncArchives(config ConfigMap) {
 		if value == "0" {
 			continue
 		}
-
-		ext := ""
-		if strings.Contains(archiveName, "nicm") || strings.Contains(archiveName, "run") {
-			ext = ""
-		} else {
-			ext = "externals"
-		}
-
-		clientArchivePath := consts.ClientRootDir + "\\" + archiveName + consts.ArchiveExtension
-
-		clientPath := ""
-		fullClientPath := ""
-		sourceArchivePath := ""
-		if ext != "" {
-			fullClientPath = consts.ClientRootDir + "\\" + ext + "\\" + archiveName
-			clientPath = consts.ClientRootDir + "\\" + ext + "\\"
-			sourceArchivePath = consts.RepoRootDir + "\\" + ext + "\\" + archiveName + consts.ArchiveExtension
-		} else {
-			fullClientPath = consts.ClientRootDir + "\\" + archiveName
-			clientPath = consts.ClientRootDir + "\\"
-			sourceArchivePath = consts.RepoRootDir + "\\" + archiveName + consts.ArchiveExtension
-		}
-		//color.Yellow("Copying %s from source: %s, to: %s, please wait...\n", archiveName, sourceArchivePath, consts.ClientRootDir)
-		copyCommand(sourceArchivePath, consts.ClientRootDir)
-
-		if _, err := os.Stat(fullClientPath); err == nil {
-			_ = os.RemoveAll(fullClientPath)
-		}
-		_ = os.MkdirAll(fullClientPath, os.ModePerm)
-
-		unzip(clientArchivePath, clientPath, archiveName)
-		_ = os.Remove(clientArchivePath)
+		wg.Add(1)
+		go syncArchive(archiveName)
 	}
+	wg.Wait()
+}
+
+func syncArchive(archiveName string) {
+	ext := getMainDir(archiveName)
+
+	clientArchivePath := consts.ClientRootDir + "\\" + archiveName + consts.ArchiveExtension
+
+	clientPath, fullClientPath, sourceArchivePath := getPaths(ext, archiveName)
+
+	color.Yellow("Copying %s, please wait...\n", archiveName)
+	copyCommand(sourceArchivePath, consts.ClientRootDir)
+
+	if _, err := os.Stat(fullClientPath); err == nil {
+		_ = os.RemoveAll(fullClientPath)
+	}
+	_ = os.MkdirAll(fullClientPath, os.ModePerm)
+
+	unzip(clientArchivePath, clientPath, archiveName)
+	_ = os.Remove(clientArchivePath)
+	wg.Done()
+}
+
+func getMainDir(archiveName string) string {
+	ext := ""
+	if strings.Contains(archiveName, "nicm") || strings.Contains(archiveName, "run") {
+		ext = ""
+	} else {
+		ext = "externals"
+	}
+	return ext
+}
+
+func getPaths(mainDir, archiveName string) (string, string, string) {
+	clientPath := ""
+	fullClientPath := ""
+	sourceArchivePath := ""
+	if mainDir != "" {
+		fullClientPath = consts.ClientRootDir + "\\" + mainDir + "\\" + archiveName
+		clientPath = consts.ClientRootDir + "\\" + mainDir + "\\"
+		sourceArchivePath = consts.RepoRootDir + "\\" + mainDir + "\\" + archiveName + consts.ArchiveExtension
+	} else {
+		fullClientPath = consts.ClientRootDir + "\\" + archiveName
+		clientPath = consts.ClientRootDir + "\\"
+		sourceArchivePath = consts.RepoRootDir + "\\" + archiveName + consts.ArchiveExtension
+	}
+
+	return clientPath, fullClientPath, sourceArchivePath
 }
 
 func unzip(archivePath, path, zipName string) {
@@ -136,6 +156,7 @@ func unzip(archivePath, path, zipName string) {
 }
 
 func copyCommand(src, dest string) {
+
 	cmd := exec.Command("cmd.exe", "/C", "copy", src, dest)
 	_ = cmd.Run()
 }
@@ -168,6 +189,7 @@ func checkNicmFile(filePath string) {
 		}
 		txt := string(value)
 		if txt == "done" {
+			_ = os.Remove(filePath)
 			return
 		}
 	}
@@ -183,21 +205,6 @@ func executeCommand(args ...string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func deleteOldNicmFiles() {
-	rootPath := fmt.Sprintf("%s\\%s", consts.ClientRootDir, consts.NicmPathToFile)
-
-	fileName := "nicm_"
-	err := filepath.Walk(rootPath, func(path string, info fs.FileInfo, err error) error {
-		if !info.IsDir() {
-			if strings.HasPrefix(info.Name(), fileName) {
-				_ = os.Remove(path)
-			}
-		}
-		return nil
-	})
-	fmt.Println(err.Error())
 }
 
 func SyncWithRepo(config ConfigMap) {
